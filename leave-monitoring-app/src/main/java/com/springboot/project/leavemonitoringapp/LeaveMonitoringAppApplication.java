cd996ac74364d5c7028d9dafa111f49c119cc6b8
Habibur Rahman <habiburkuetacbd@gmail.com>
2024-04-21 12:31:09 +0600

    started spring boot project with leave issue

---
+++ leave-monitoring-app/src/main/java/com/springboot/project/leavemonitoringapp/LeaveMonitoringAppApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.project.leavemonitoringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaveMonitoringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaveMonitoringAppApplication.class, args);
+	}
+
+}
---
+++ leave-monitoring-app/src/test/java/com/springboot/project/leavemonitoringapp/LeaveMonitoringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.project.leavemonitoringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaveMonitoringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
